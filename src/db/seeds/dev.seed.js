f015823e5b81c4c1a8d4519750439021c8ac756d
Emmanuel Rodríguez <rorem.94.developer@gmail.com>
2022-09-26 19:04:59 -0500

    feat: ✨added seed and product mock

---
+++ src/db/seeds/dev.seed.js
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+import { complete, fail } from '@utils/logger.util'
+
+import products from '@mocks/products.mock'
+
+const _prisma = new PrismaClient()
+
+const _createBatch = (model, collection) =>
+    Promise.all(
+        collection.map(async (item) => {
+            await _prisma[model].create({ data: item })
+        })
+    )
+        .then(() => complete(`${model} created`))
+        .catch((error) => {
+            fail(`${model} failed`, error.message)
+        })
+
+;(async () => {
+    await _createBatch('product', products)
+})().finally(async () => {
+    await _prisma.$disconnect()
+})
