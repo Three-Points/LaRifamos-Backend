169233d00a7290d22692b066b75f8246347b5542
Emmanuel Rodríguez <rorem.94.developer@gmail.com>
2022-10-23 01:15:31 -0500

    feat: ✨added account controller file

---
+++ src/controllers/Account.controller.js
@@ -0,0 +1,99 @@
+import AccountModel from '@models/Account.model'
+import ErrorServer from '@controllers/ErrorServer.controller'
+
+export default class Account {
+    #model = new AccountModel()
+
+    /**
+     * @description
+     * Find an account.
+     * If liked is provided, it will be included liked raffles.
+     * @param query Query request object.
+     * @param options Options request object.
+     * @throws SERVER, id parameter is mandatory.
+     * @throws NOT_FOUND, Account not found.
+     * @returns Account, optional includes. */
+    async findAccount({ id, liked, shared }, options = {}) {
+        if (!id) throw new ErrorServer('SERVER', 'id parameter is mandatory')
+        const account = await this.#model.findUnique(
+            {
+                id,
+                liked,
+                shared,
+            },
+            options
+        )
+        if (!account) throw new ErrorServer('NOT_FOUND', 'Account not found')
+        const { include } = options
+        return {
+            name: account.name,
+            ...((include?.liked || liked) && { liked: account.liked }),
+            ...((include?.shared || shared) && { shared: account.shared }),
+        }
+    }
+
+    /**
+     * @description
+     * Like/Unlike an account with a raffle as connection/disconnection.
+     * Query by:
+     *  - accountId, ID of account.
+     * Payload by:
+     *  - raffleId, ID of raffle.
+     * @param query Query request object.
+     * @param payload Payload request object.
+     * @throws SERVER, id parameter is mandatory.
+     * @throws SERVER, raffleId parameter is mandatory.
+     * @throws NOT_FOUND, Account not found. */
+    async rateRaffle({ accountId }, { raffleId }) {
+        if (!accountId)
+            throw new ErrorServer('SERVER', 'id parameter is mandatory')
+        if (!raffleId)
+            throw new ErrorServer('SERVER', 'raffleId parameter is mandatory')
+
+        const { liked } = await this.findAccount({
+            id: accountId,
+            liked: { id: raffleId },
+        })
+        if (!liked?.length) {
+            await this.#model.connect(
+                { id: accountId },
+                { likedRaffleId: raffleId }
+            )
+        } else {
+            await this.#model.disconnect(
+                { id: accountId },
+                { likedRaffleId: raffleId }
+            )
+        }
+    }
+
+    /**
+     * @description
+     * To share, connect an account with a raffle.
+     * Query by:
+     *  - accountId, ID of account.
+     * Payload by:
+     *  - raffleId, ID of raffle.
+     * @param query Query request object.
+     * @param payload Payload request object.
+     * @throws SERVER, id parameter is mandatory.
+     * @throws SERVER, raffleId parameter is mandatory.
+     * @throws NOT_FOUND, Account not found. */
+    async shareRaffle({ accountId }, { raffleId }) {
+        if (!accountId)
+            throw new ErrorServer('SERVER', 'id parameter is mandatory')
+        if (!raffleId)
+            throw new ErrorServer('SERVER', 'raffleId parameter is mandatory')
+
+        const { shared } = await this.findAccount({
+            id: accountId,
+            shared: { id: raffleId },
+        })
+        if (!shared?.length) {
+            await this.#model.connect(
+                { id: accountId },
+                { sharedRaffleId: raffleId }
+            )
+        }
+    }
+}
