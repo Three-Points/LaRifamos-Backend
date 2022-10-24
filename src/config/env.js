7507e9ba6ebc9e7a922a0b5c45599ad017592c1e
Emmanuel Rodríguez <rorem.94.developer@gmail.com>
2022-10-24 16:02:49 -0500

    feat: ✨added auth abstraction
    Updated env to support tokens
    Added auth middleware to verify tokens and login
    Added routes for authentication through login and verify identity

--- src/config/env.js
+++ src/config/env.js
@@ -6,6 +6,7 @@ dotenvExpand.expand(env)
 
 export const MODE = process.env.MODE || process.env.NODE_ENV
 export const PORT = process.env.PORT
+export const API_TOKEN = process.env.API_TOKEN
 export const JWT_TOKEN = process.env.JWT_TOKEN
 
 export const DB_URL = process.env.DB_URL
