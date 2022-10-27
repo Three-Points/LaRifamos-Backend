932403779a412be4bdad16f12f570e8ff9463f0e
manuel <manuel.martinez.casas@gmail.com>
2022-10-27 21:52:47 +0000

    feat: add route raffle by id

--- src/routes/raffle.route.js
+++ src/routes/raffle.route.js
@@ -2,10 +2,29 @@ import { Router } from 'express'
 import { verifyJWTToken } from '@middlewares/auth.middleware'
 import { success } from '@middlewares/response.middleware'
 import wrapper from '@middlewares/wrapper.middleware'
-import { postRateRaffle, postShareRaffle } from '@services/raffle.service'
+import {
+    getRaffle,
+    postRateRaffle,
+    postShareRaffle,
+} from '@services/raffle.service'
 import { info } from '@utils/logger.util'
 
 const router = Router()
+
+/**
+ * @name GET
+ * @description Gets project info as health route. */
+router.get(
+    '/:id',
+    //wrapper(verifyAPIToken),
+    wrapper(async (req, res, next) => {
+        info('GET /raffle/:id')
+        const { id } = req.params
+        res.locals.data = await getRaffle(Number(id))
+        next()
+    }),
+    success
+)
 
 /**
  * @name POST
